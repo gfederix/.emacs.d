43c62fb82f7f53ab062b6b7de664d2cb3f0fd01c
Fedor P. Goncharov <gfederix@gmail.com>
2021-01-04 21:22:47 +0700

    update

--- init.el
+++ init.el
@@ -1,3 +1,10 @@
+;; Global configurations
+(tool-bar-mode 0)
+(setq inhibit-startup-message t)
+(global-linum-mode)
+(show-paren-mode 1)
+
+;; Straight with use-package
 (defvar bootstrap-version)
 (let ((bootstrap-file
        (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
@@ -12,11 +19,6 @@
   (load bootstrap-file nil 'nomessage))
 (straight-use-package 'use-package)
 
-;; Global configurations
-(setq inhibit-startup-message t)
-(tool-bar-mode 0)
-(show-paren-mode 1)
-(global-linum-mode)
 
 ;; Theme
 ;; (use-package material-theme :straight t)
@@ -36,18 +38,27 @@
 (which-key-mode)
 
 ;; Helm
-(use-package helm :straight t)
-(helm-mode 1)
-(global-set-key (kbd "M-x") 'helm-M-x)
-(global-set-key (kbd "C-x C-f") 'helm-find-files)
+(use-package helm
+  :straight t
+  :init (helm-mode 1)
+  :bind (("M-x" . helm-M-x)
+	("C-x C-f" . helm-find-files)))
+
 ;; Git
 (use-package magit :straight t)
+(use-package projectile
+  :straight t
+  :ensure t
+  :init (projectile-mode +1)
+  :bind (( "s-p" . projectile-command-map)
+	 ("C-c p" . projectile-command-map)
+	 ))
 
-;; flycheck
-;; (use-package flycheck
-;;   :straight t
-;;   :ensure t
-;;   :init (global-flycheck-mode))
+;; Flycheck
+(use-package flycheck
+  :straight t
+  :ensure t
+  :init (global-flycheck-mode))
 
 ;; Python
 ;; https://www.emacswiki.org/emacs/PythonProgrammingInEmacs
@@ -56,10 +67,11 @@
   :straight t
   :ensure t
   :init (elpy-enable))
-;; (when (load "flycheck" t t)
-;;   (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
-;;   (add-hook 'elpy-mode-hook 'flycheck-mode))
-(use-package pyvenv :straight t)
+(when (load "flycheck" t t)
+  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
+  (add-hook 'elpy-mode-hook 'flycheck-mode)) ;activate flycheck
+
+;; (use-package pyvenv :straight t)
 ;; (use-package virtualenvwrapper :straight t)
 ;; (use-package pyenv-mode :straight t)
 
